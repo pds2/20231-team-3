51ea572aabb2c172bf244824023a69852fa44749
Lucas-Rezende <lucasmomede@yahoo.com.br>
2023-06-08 19:13:55 -0300

    Estrutura de pastas

---
+++ include/livro.hpp
@@ -0,0 +1,11 @@
+#ifndef LIVRO_HPP
+#define LIVRO_HPP
+
+#include <iostream>
+#include <set>
+#include <string>
+
+std::set<string> Livros;
+
+
+#endif
---
+++ include/usuario.hpp
@@ -0,0 +1,14 @@
+#ifndef USUARIO_HPP
+#define USUARIO_HPP
+
+class Usuario
+{
+private:
+    
+public:
+    
+};
+
+//lista de usuários
+
+#endif
