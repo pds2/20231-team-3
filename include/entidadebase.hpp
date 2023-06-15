d420e3f9895272f910676b2961f4abb4224d34d3
Lucas-Rezende <lucasmomede@yahoo.com.br>
2023-06-15 20:56:09 -0300

    Criando .cpp de Entidadebase Versão 2

--- include/entidadebase.hpp
+++ include/entidadebase.hpp
@@ -1,12 +1,13 @@
 #ifndef ENTIDADE_BASE_HPP
 #define ENTIDADE_BASE_HPP
 
+#include <iostream>
 #include <string>
 
-//TODO: Implementar
+// TODO: Implementar
 /**
-  * Decidir critérios para os nomes, emails e senhas
-*/
+ * Decidir critérios para os nomes, emails e senhas
+ */
 
 /**
  * @brief Classe responsavel por atuar como
@@ -15,27 +16,27 @@
 class Entidadebase
 {
 private:
-    std::string _nome;
-    std::string _email;
-    unsigned int _id;
-    std::string _senha;
+    std::string _nome const;
+    std::string _email const;
+    unsigned int _id const;
+    std::string _senha const;
+
 public:
     Entidadebase(
         std::string nome,
         std::string email,
         unsigned int id,
-        std::string senha
-    );
+        std::string senha);
 
     std::string getNome() const;
     std::string getEmail() const;
     unsigned int getId() const;
     std::string getSenha() const;
 
-    void setNome(const std::string& nome);
-    void setEmail(const std::string& email);
+    void setNome(const std::string &nome);
+    void setEmail(const std::string &email);
     void setId(unsigned int id);
-    void setSenha(const std::string& senha);
+    void setSenha(const std::string &senha);
 };
 
 #endif
--- src/entidadebase.cpp
+++ src/entidadebase.cpp
@@ -1,24 +1,44 @@
 #include "../include/entidadebase.hpp"
 
-Entidadebase::Entidadebase() {
-    std::string nome,
-    std::string email,
-    unsigned int id,
-    std::string senha
-}: Entidadebase(nome), Entidadebase(email), Entidadebase(id), Entidadebase(senha) { }
+Entidadebase::Entidadebase(const std::string &nome, const std::string &email, unsigned int id, const std::string &senha)
+    : _nome(nome), _email(email), _id(id), _senha(senha) {}
 
-std::string Entidadebase::getNome() const {
+std::string Entidadebase::getNome() const
+{
     return _nome;
 }
 
-std::string Entidadebase::getEmail() const {
+std::string Entidadebase::getEmail() const
+{
     return _email;
 }
 
-unsigned int Entidadebase::getId() const {
+unsigned int Entidadebase::getId() const
+{
     return _id;
 }
 
-std::string Entidadebase::getSenha() const {
+std::string Entidadebase::getSenha() const
+{
     return _senha;
+}
+
+void Entidadebase::setNome(const std::string &nome)
+{
+    _nome = nome;
+}
+
+void Entidadebase::setEmail(const std::string &email)
+{
+    _email = email;
+}
+
+void Entidadebase::setId(unsigned int id)
+{
+    _id = id;
+}
+
+void Entidadebase::setSenha(const std::string &senha)
+{
+    _senha = senha;
 }
