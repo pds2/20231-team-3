7813ffc6a108ecc243b899bb34577104622ea078
lucas <lucaswnn@hotmail.com>
2023-06-30 23:50:49 -0300

    Novo teste para o banco de dados Criada pasta program para conter main de testes Necessário modificar Makefile
     Changes to be committed:
    	new file:   program/tester.cpp
    	new file:   tests/teste_db.cpp

---
+++ tests/teste_db.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+
+#include "doctest.h"
+#include "definicoes.hpp"
+#include "db_acervo.hpp"
+#include "db_usuarios.hpp"
+
+TEST_CASE("teste de criação de banco de dados")
+{
+    remove(bbt_def::sql::NOME_DB.c_str());
+
+    auto db_us = DbUsuarios();
+    auto db_ac = DbAcervo();
+}
+
+TEST_CASE("teste de inserção de um livro")
+{
+    auto db_us = DbUsuarios();
+    auto db_ac = DbAcervo();
+    db_ac.del_linhas();
+    auto livro1 = Livro("titulo1", "autor1", "genero1", "resumo1", "idioma1", 10, 2023, 5);
+    auto livro2 = Livro("titulo2", "autor2", "genero2", "resumo2", "idioma2", 10, 2023, 5);
+
+    db_ac.inserir_linha(livro1);
+    db_ac.inserir_linha(livro2);
+
+    auto livro1_consulta = std::get<1>(
+        db_ac.consulta("titulo1", bbt_def::sql::schema_acervo::titulo).back()
+    );
+
+    auto livro2_consulta = std::get<1>(
+        db_ac.consulta("titulo2", bbt_def::sql::schema_acervo::titulo).back()
+    );
+
+    CHECK_EQ(livro1.getTitulo(), livro1_consulta.getTitulo());
+    CHECK_EQ(livro1.getAutor(), livro1_consulta.getAutor());
+    CHECK_EQ(livro1.getGenero(), livro1_consulta.getGenero());
+    CHECK_EQ(livro1.getResumo(), livro1_consulta.getResumo());
+    CHECK_EQ(livro1.getIdioma(), livro1_consulta.getIdioma());
+    CHECK_EQ(livro1.getNumPaginas(), livro1_consulta.getNumPaginas());
+    CHECK_EQ(livro1.getAno(), livro1_consulta.getAno());
+    CHECK_EQ(livro1.getAvaliacao(), livro1_consulta.getAvaliacao());
+
+    CHECK_EQ(livro2.getTitulo(), livro2_consulta.getTitulo());
+    CHECK_EQ(livro2.getAutor(), livro2_consulta.getAutor());
+    CHECK_EQ(livro2.getGenero(), livro2_consulta.getGenero());
+    CHECK_EQ(livro2.getResumo(), livro2_consulta.getResumo());
+    CHECK_EQ(livro2.getIdioma(), livro2_consulta.getIdioma());
+    CHECK_EQ(livro2.getNumPaginas(), livro2_consulta.getNumPaginas());
+    CHECK_EQ(livro2.getAno(), livro2_consulta.getAno());
+    CHECK_EQ(livro2.getAvaliacao(), livro2_consulta.getAvaliacao());
+}
+
+TEST_CASE("Teste de consulta")
+{
+    auto db_us = DbUsuarios();
+    auto db_ac = DbAcervo();
+    db_ac.del_linhas();
+
+    auto livro1 = Livro("titulo1", "autor1", "genero1", "resumo1", "idioma1", 10, 2023, 5);
+    auto livro2 = Livro("titulo2", "autor1", "genero2", "resumo2", "idioma2", 10, 2023, 5);
+
+    db_ac.inserir_linha(livro1);
+    db_ac.inserir_linha(livro2);
+
+    auto autor_consulta = db_ac.consulta("autor1", bbt_def::sql::schema_acervo::autor);
+    CHECK_EQ(2, autor_consulta.size());
+
+    unsigned int id_livro_titulo1;
+    for(auto tuple : autor_consulta)
+    {
+        auto livro = std::get<1>(tuple);
+        if(livro.getTitulo() == "titulo1") id_livro_titulo1 = std::get<0>(tuple);
+    }
+
+    db_ac.sobrescrever_em_id(
+        std::string("2023-06-29 00:00:00"),
+        bbt_def::sql::schema_acervo::data_aluguel,
+        id_livro_titulo1);
+    
+    auto data_livro_titulo1 = std::get<2>
+    (
+        db_ac.consulta("2023-06-29 00:00:00", bbt_def::sql::schema_acervo::data_aluguel).back()
+    ).text_data.back();
+    
+    CHECK_EQ("2023-06-29 00:00:00", data_livro_titulo1);
+}
+
+TEST_CASE("Teste da chave cruzada entre tabela usuários e tabela acervo")
+{
+    auto db_ac = DbAcervo();
+    auto db_us = DbUsuarios();
+
+    db_ac.del_linhas();
+    db_us.del_linhas();
+
+    auto livro1 = Livro("titulo1", "autor1", "genero1", "resumo1", "idioma1", 10, 2023, 5);
+    auto livro2 = Livro("titulo2", "autor1", "genero2", "resumo2", "idioma2", 10, 2023, 5);
+    
+    auto user1 = Usuario("user1", "senha", "email1");
+    auto user2 = Usuario("user2", "senha", "email2");
+    auto user3 = Usuario("user3", "senha", "email3");
+
+    db_us.inserir_linha(user1);
+    db_us.inserir_linha(user2);
+    db_us.inserir_linha(user3);
+
+    db_ac.inserir_linha(livro1);
+    db_ac.inserir_linha(livro2);
+
+    auto id_livro1 = std::get<0>
+    (
+        db_ac.consulta("titulo1", bbt_def::sql::schema_acervo::titulo).back()
+    );
+
+    auto id_user2 = std::get<0>
+    (
+        db_us.consulta("user2", bbt_def::sql::schema_usuarios::nome).back()
+    );
+
+    db_ac.sobrescrever_em_id(id_user2, bbt_def::sql::schema_acervo::posse_id, id_livro1);
+
+    auto id_posse_livro1 = std::get<2>
+    (
+        db_ac.consulta("titulo1", bbt_def::sql::schema_acervo::titulo).back()
+    ).long_data.at(0);
+
+    CHECK_EQ(id_posse_livro1, id_user2);
+
+    db_us.deletar_linha_id(id_user2);
+
+    auto livro1_adt_data = std::get<2>
+    (
+        db_ac.consulta("titulo1", bbt_def::sql::schema_acervo::titulo).back()
+    ).long_data;
+
+    CHECK_EQ(livro1_adt_data.size(), 0);
+}
+
+TEST_CASE("teste de sobrescrita de linhas")
+{
+    auto db_ac = DbAcervo();
+    db_ac.del_linhas();
+
+    auto livro1 = Livro("titulo1", "autor1", "genero1", "resumo1", "idioma1", 10, 2020, 0.0);
+    auto livro2 = Livro("titulo2", "autor1", "genero1", "resumo2", "idioma1", 10, 2020, 0.0);
+
+    db_ac.inserir_linha(livro1);
+
+    auto id_livro1 = std::get<0>
+    (
+        db_ac.consulta("titulo1", bbt_def::sql::schema_acervo::titulo).back()
+    );
+
+    db_ac.sobrescrever_em_id(livro2, id_livro1);
+
+    auto livro_subst = std::get<1>
+    (
+        db_ac.consulta(id_livro1, bbt_def::sql::id).back()
+    );
+
+    CHECK_EQ("titulo2", livro_subst.getTitulo());
+}
